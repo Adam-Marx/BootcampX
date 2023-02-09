1a9d5b853498170a1c1b3fcbf6edf53581f49e4f
Adam Marx <adamalexmarx@gmail.com>
2023-02-08 20:51:18 -0800

    Protect versus SQL injection

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,5 +1,6 @@
-const pool = require('/students');
+const { pool } = require('./students');
 
+const cohortName = process.argv[2];
 
 pool.query(`
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
@@ -7,9 +8,9 @@ FROM teachers
 JOIN assistance_requests ON teachers.id = assistance_requests.teacher_id
 JOIN students ON students.id = assistance_requests.student_id 
 JOIN cohorts ON cohorts.id = students.id 
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name = $1 || 'JUL02'
 ORDER BY teacher;
-`)
+`, [cohortName])
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
