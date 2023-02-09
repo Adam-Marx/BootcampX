cfbb83bbb2eed64a09e3b101f063e1aca727c457
Adam Marx <adamalexmarx@gmail.com>
2023-02-08 20:22:51 -0800

    Database query using node postgres

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,17 @@
+const pool = require('/students');
+
+
+pool.query(`
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teachers.id = assistance_requests.teacher_id
+JOIN students ON students.id = assistance_requests.student_id 
+JOIN cohorts ON cohorts.id = students.id 
+WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+ORDER BY teacher;
+`)
+.then(res => {
+  res.rows.forEach(row => {
+    console.log(`${row.cohort}: ${row.teacher}`);
+  })
+});
